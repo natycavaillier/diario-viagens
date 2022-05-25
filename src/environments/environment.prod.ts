607e5f55c7c852bf12a516e7c565badf5612bfbe
jrcodev <jr.cod.dev@gmail.com>
2022-05-25 13:43:37 -0300

    install: setup firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'diario-viagens-6c73b',
+    appId: '1:167645777431:web:a78fb2a6737b6bc1345652',
+    storageBucket: 'diario-viagens-6c73b.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBGTgkqJHUv1O_bWq7EfAJ1eUUnegEDFnw',
+    authDomain: 'diario-viagens-6c73b.firebaseapp.com',
+    messagingSenderId: '167645777431',
+  },
   production: true
 };
