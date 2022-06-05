6effa077a8cfa20a15c96062a768013c1de8c630
Taisaferreiraflavio <taisaferreiraflavio@gmail.com>
2022-06-05 20:22:22 -0300

    Politica de Privacidade

---
+++ src/app/home/components/politica/politica.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PoliticaComponent } from './politica.component';
+
+describe('PoliticaComponent', () => {
+  let component: PoliticaComponent;
+  let fixture: ComponentFixture<PoliticaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PoliticaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PoliticaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
