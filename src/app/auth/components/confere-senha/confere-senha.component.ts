5f52601d5278cee62ccf721375e61e67f351ede3
Taisaferreiraflavio <taisaferreiraflavio@gmail.com>
2022-06-08 10:54:03 -0300

    Um passo de cada vez Stepper

---
+++ src/app/auth/components/confere-senha/confere-senha.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confere-senha',
+  templateUrl: './confere-senha.component.html',
+  styleUrls: ['./confere-senha.component.scss']
+})
+export class ConfereSenhaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
